131045bad8ba68b715397087762b788e0d3f14a1
Logan Thomas <logan.thomas@forsta.com>
2023-01-19 01:59:44 -0700

    added grommet, and started building design for calendar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { grommet, Grommet } from "grommet";
+import type { AppProps } from "next/app";
+
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
+    },
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Grommet theme={theme} full>
+      <Component {...pageProps} />
+    </Grommet>
+  );
 }
